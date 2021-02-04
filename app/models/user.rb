5f0d9ad211fcda5ccf407d70ad2aff0aa866bee8
shingo04 <shingobb04@gmail.com>
2021-02-04 16:01:09 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with: /\A[0-9]+\z/ } do
+      validates :age
+      validates :height
+      validates :weight
+      validates :bmr
+    end
+  end
+end
